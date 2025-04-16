c5d7b8ad74dd91eb5ca8363539150afcba10044f
Marcus Quach <marcusquach07@gmail.com>
2025-04-16 14:50:42 -0600

    Created

---
+++ Foundation of C/DataType_and_TypeModifiers/DT&TM.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+        // unsigned int to add a int data type
+        // to store values that arent negative
+        unsigned int ui = -100;
+        printf("ui: %u", ui);
+        return 0;
+}
